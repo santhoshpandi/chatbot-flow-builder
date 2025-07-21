b065ca7b5db7b67dc9219b78a9c4e98a367e848a
santhoshpandi <santhoshpandi543@gmail.com>
2025-07-21 13:43:36 +0530

    fitView added

--- src/components/FlowBuilder/FlowBuilder.jsx
+++ src/components/FlowBuilder/FlowBuilder.jsx
@@ -82,6 +82,7 @@ export default function FlowBuilder({ setSelectedNode }) {
         nodeTypes={nodeTypes}
         onNodeClick={onNodeClick}
         onPaneClick={onPaneClick}
+        fitView
       >
         <Background />
         <Controls />
